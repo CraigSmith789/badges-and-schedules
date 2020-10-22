4d16000a6795b9d40545ba441fe8828b40bd4d51
CraigSmith789 <craig789@gmail.com>
2020-10-22 09:33:21 -0400

    commit tree 6c4899c7de2d7919e1260eaf6140bc5bab948790

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+        array.each do |name|
+            new_array << "Hello, my name is #{name}."
+    end 
+    new_array
+end
+
+def assign_rooms(array)
+    rooms_array = []
+    array.each_with_index { |name, idx| rooms_array << "Hello, #{name}! You'll be assigned to room #{idx+1}!" }
+    rooms_array
+end
+
+def printer(array) 
+    badges = batch_badge_creator(array)
+    badges.each{ |badge| puts "#{badge}"}
+
+    rooms = assign_rooms(array)
+    rooms.each{|room| puts"#{room}"}
+
+end
